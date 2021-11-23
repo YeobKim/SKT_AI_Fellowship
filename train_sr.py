c7c0a27f62bcdd676e99642218f4c04a8fedf346
Jaeyeob KIM <athurk94111@gmail.com>
2021-11-24 00:54:53 +0900

    Update train_sr.py

--- train_sr.py
+++ train_sr.py
@@ -4,7 +4,7 @@ import torchvision.utils as utils
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
 from utils.utils import *
-from models.srnet import *
+from models.srnet_ad import *
 from matplotlib import pyplot as plt
 import torchvision.transforms as transforms
 from PIL import ImageFilter
@@ -20,9 +20,11 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 parser = argparse.ArgumentParser(description="SKT AI Fellowship SKSAK")
 parser.add_argument("--preprocess", type=bool, default=False, help='run prepare_data or not')
 parser.add_argument("--batchSize", type=int, default=1, help="Training batch size")
-parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
-parser.add_argument("--resume_epochs", type=int, default=52, help="Number of training epochs When training resume")
-parser.add_argument("--lr", type=float, default=2e-5, help="Initial learning rate")
+parser.add_argument("--epochs", type=int, default=60, help="Number of training epochs")
+parser.add_argument("--resume_epochs", type=int, default=0, help="Number of training epochs When training resume")
+parser.add_argument("--decaystart_epochs", type=int, default=20, help="Number of training epochs When training resume")
+parser.add_argument("--lr", type=float, default=0.5e-4, help="Initial learning rate")
+parser.add_argument("--step", type=int, default=10, help="When to decay learning rate; should be less than epochs")
 parser.add_argument("--last_lr", type=float, default=0.5e-6, help="Last learning rate")
 parser.add_argument("--outf", type=str, default="logs", help='path of log files')
 parser.add_argument("--patchsize", type=int, default=512, help='patch size of image')
@@ -61,20 +63,15 @@ def main():
     criterion2.cuda()
 
     # Optimizer
-    optimizer = optim.AdamW(model.parameters(), lr=opt.lr)
-
-    # Scheduler
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=60, eta_min=opt.last_lr)
-    # scheduler = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=3, after_scheduler=scheduler_cos)
-    scheduler.step()
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
     # training
     writer = SummaryWriter(opt.outf)
     step = 0
 
     # if you want to train resume
-    load = torch.load(os.path.join(opt.logdir, 'SKT_AI_sr_52_0.029.pth'))
-    model.load_state_dict(load)
+    # load = torch.load(os.path.join(opt.logdir, 'SKT_AI_line_25_0.0377.pth'))
+    # model.load_state_dict(load)
     # optimizer.load_state_dict(load['optimizer'])
 
     start_time = datetime.now()
@@ -83,10 +80,13 @@ def main():
 
     for epoch in range(opt.resume_epochs, opt.epochs):
         # setting lr
-        if (epoch + 1) < 63:
-            current_lr = scheduler.get_last_lr()[0]
+        if (epoch + 1) < opt.decaystart_epochs:
+            current_lr = opt.lr
         else:
-            current_lr = opt.last_lr
+            current_lr = opt.lr * (0.5 ** (((epoch + 1) - (opt.decaystart_epochs - opt.step)) // opt.step))
+            # current_lr = current_lr * 0.5
+            if current_lr <= 0.5e-6:
+                current_lr = 0.5e-6
 
         # set learning rate
         for param_group in optimizer.param_groups:
@@ -100,7 +100,7 @@ def main():
             model.zero_grad()
             optimizer.zero_grad()
 
-            randomnum = random.randint(0, 2)
+            noise_level = random.randint(20, 30)
 
             # Make Edge Image.
             original_edge = torch.FloatTensor(degraded_train.shape[0], 3, opt.patchsize, opt.patchsize)
@@ -108,16 +108,9 @@ def main():
             for j in range(degraded_train.shape[0]):
                 original_edge[j] = toTensor((toPILImg(gt_train[j]).filter(ImageFilter.FIND_EDGES)))
 
-            if randomnum == 0:
-                noise = torch.FloatTensor(degraded_train.shape[0], 1, degraded_train.shape[2], degraded_train.shape[3]).normal_(mean=0, std=5 / 255.)
-                degraded_img = degraded_train + noise
-            elif randomnum == 1:
-                noise = torch.FloatTensor(degraded_train.shape[0], 1, degraded_train.shape[2], degraded_train.shape[3]).normal_(mean=0, std=10 / 255.)
-                degraded_img = degraded_train + noise
-            else:
-                # noise = torch.FloatTensor(degraded_train.size()).normal_(mean=0, std=15 / 255.)
-                noise = torch.FloatTensor(degraded_train.shape[0], 1, degraded_train.shape[2], degraded_train.shape[3]).normal_(mean=0, std=15 / 255.)
-                degraded_img = degraded_train + noise
+
+            noise = torch.FloatTensor(degraded_train.shape[0], 3, degraded_train.shape[2], degraded_train.shape[3]).normal_(mean=0,std=noise_level/255.)
+            degraded_img = degraded_train + noise
 
 
             degraded_img, gt_train = Variable(degraded_img.cuda()), Variable(gt_train.cuda())
@@ -125,12 +118,9 @@ def main():
 
             out_train = model(degraded_img)
 
-            if epoch < 63:
-                img_loss = np.sum([criterion(out_train[j], gt_train) for j in range(len(out_train)-1)])
-                edge_loss = criterion(out_train[-1], original_edge)
-            else:
-                img_loss = np.sum([criterion2(out_train[j], gt_train) for j in range(len(out_train)-1)])
-                edge_loss = criterion(out_train[-1], original_edge)
+            # img_loss = np.sum([criterion(out_train[j], gt_train) for j in range(len(out_train)-1)])
+            img_loss = criterion(out_train[0], gt_train) + criterion(out_train[1], gt_train) + criterion(out_train[2], gt_train)
+            edge_loss = criterion(out_train[-1], original_edge)
 
             loss = img_loss + 0.5 * edge_loss
             loss_val += loss.item()
@@ -140,8 +130,6 @@ def main():
 
             # results
             model.eval()
-            # out_train, stage2_img, stage1_img, edge = model(degraded_train)
-            # out_train = torch.clamp(out_train, 0., 1.)
             out_train = model(degraded_img)
             out_img = torch.clamp(out_train[0], 0., 1.)
             edge_img = out_train[-1]
@@ -157,13 +145,10 @@ def main():
                 Imgn = utils.make_grid(degraded_img[0].data, nrow=8, normalize=True, scale_each=True)
                 edgeImg = utils.make_grid(edge_img[0].data, nrow=8, normalize=True, scale_each=True)
                 Irecon = utils.make_grid(out_img[0].data, nrow=8, normalize=True, scale_each=True)
-                # writer.add_image('clean image', Img, epoch)
-                # writer.add_image('noisy image', Imgn, epoch)
-                # writer.add_image('reconstructed image', Irecon, epoch)
 
                 # Compare clean, degraded, restoration image
                 fig = plt.figure()
-                fig.suptitle('SKT_AI_ %d' % (epoch + 1))
+                fig.suptitle('SKT_Ringing_line_ %d' % (epoch + 1))
                 rows = 2
                 cols = 2
 
@@ -199,13 +184,10 @@ def main():
         midtime = datetime.now() - start_time
         print(midtime)
 
-        # torch.save({'state_dict': model.state_dict(),
-        #             'optimizer': optimizer.state_dict()
-        #             }, os.path.join(opt.outf, 'SKT_AI_' + str(epoch + 1) + "_" + str(round(loss_val, 4)) + '.pth'))
         torch.save(model.state_dict(),
-                   os.path.join(opt.outf, 'SKT_AI_sr_' + str(epoch + 1) + "_" + str(round(loss_val, 4)) + '.pth'))
+                   os.path.join(opt.outf, 'SKT_AI_line_' + str(epoch + 1) + "_" + str(round(loss_val, 4)) + '.pth'))
 
-        scheduler.step()
+        # scheduler.step()
 
     end_time = datetime.now()
     print('Training Finished!!')
